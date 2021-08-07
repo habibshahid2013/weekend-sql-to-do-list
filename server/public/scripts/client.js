30e6fa484006b1df17f54b4c861080887e137fed
Hassan Shahid <habibshahid2013@gmail.com>
2021-08-06 23:41:40 -0500

    added notes in client.js

---
+++ server/public/scripts/client.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    console.log('Lets see if Jqury gonna fail me 😈');
+    //add button from Jquery here "Use click handler"
+});
+
+//Maybe set up a Click handler
+
+//Function to handle a submission
+
+//Set up Delete Function 
+
+//Set up an ADD Function 
+
+//Set UP a Refresh function
+
+//Rendering function to append material
