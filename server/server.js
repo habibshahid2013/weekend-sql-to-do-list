cde55a4f1820bb9b9267241196ab3b82df5a2687
Hassan Shahid <habibshahid2013@gmail.com>
2021-08-06 23:14:40 -0500

    installed body-parser

--- server/server.js
+++ server/server.js
@@ -6,6 +6,22 @@ const app = express();
 //Express will know where to find all of our public files "Client-side"
 app.use(express.static('./server/public'));
 
+
+//haven't defined URL yet
+//Listen for request coming to URL
+//GET
+app.get('/', function (req, res) {
+    console.log('ready to send back something');
+    
+    res.send('in all due time it will make sense');
+});
+
+//POST 
+app.post('/', function (req, res) {
+    console.log('');
+    
+} )
+
 //listen for requests
 const port = 5000;
 app.listen(port, function (){
